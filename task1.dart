fcc6776dd16639f50076325e25d2d9653f6012da
pradipdhungana99 <pradipdhungana82@gmail.com>
2024-12-03 16:18:23 +0545

    add day 2

--- task1.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  String firstName = 'Andrea';
-  String lastName = 'Bizotto';
-  final age = 36;
-  double height = 1.86;
-
-  print(
-      'Your name is $firstName $lastName, You are $age years old. You are $height tall');
-}
